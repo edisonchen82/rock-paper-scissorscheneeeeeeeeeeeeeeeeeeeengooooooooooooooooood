12dc007d18dc453650299ed5ec9cce001a8abc61
edisonchen82 <103941871+edisonchen82@users.noreply.github.com>
2022-06-15 09:51:12 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-
+def on_forever():
+    basic.show_icon(IconNames.TORTOISE)
+    basic.pause(10)
+    basic.show_icon(IconNames.HEART)
+    basic.show_leds("""
+        . . . . .
+                # # # # #
+                . . # # .
+                . # . . .
+                # # # # #
+    """)
+    basic.show_number(8)
+    basic.clear_screen()
+    basic.show_arrow(ArrowNames.NORTH_EAST)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
 basic.forever(function () {
     basic.showIcon(IconNames.Tortoise)
     basic.pause(10)
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        . . # # .
+        . # . . .
+        # # # # #
+        `)
+    basic.showNumber(8)
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.NorthEast)
 })
